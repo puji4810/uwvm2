410bfbb8aa80ceefeff72bd59055a180c21231ce
MacroModel <MacroModel_Traject@outlook.com>
2025-10-21 16:04:07 +0800

    [wasip1] refactor file descriptor setup to utilize unified storage structure for file types in fd_fdstat_set_rights tests

--- test/0008.imported/wasi/wasip1/func/fd_fdstat_set_rights_wasm64.cc
+++ test/0008.imported/wasi/wasip1/func/fd_fdstat_set_rights_wasm64.cc
@@ -40,7 +40,12 @@ int main()
     native_memory_t memory{};
     memory.init_by_page_count(1uz);
 
-    wasip1_environment<native_memory_t> env{.wasip1_memory = ::std::addressof(memory), .argv = {}, .envs = {}, .fd_storage = {}, .mount_dir_roots={}, .trace_wasip1_call = false};
+    wasip1_environment<native_memory_t> env{.wasip1_memory = ::std::addressof(memory),
+                                            .argv = {},
+                                            .envs = {},
+                                            .fd_storage = {},
+                                            .mount_dir_roots = {},
+                                            .trace_wasip1_call = false};
 
     // Prepare fd table
     env.fd_storage.opens.resize(8uz);
@@ -48,9 +53,9 @@ int main()
     // Case 0: negative fd => ebadf
     {
         auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env,
-                                                                                             static_cast<wasi_posix_fd_wasm64_t>(-1),
-                                                                                             static_cast<rights_wasm64_t>(0),
-                                                                                             static_cast<rights_wasm64_t>(0));
+                                                                                            static_cast<wasi_posix_fd_wasm64_t>(-1),
+                                                                                            static_cast<rights_wasm64_t>(0),
+                                                                                            static_cast<rights_wasm64_t>(0));
         if(ret != errno_wasm64_t::ebadf)
         {
             ::fast_io::io::perrln(::fast_io::u8err(), u8"fd_fdstat_set_rights_wasm64: expected ebadf for negative fd");
@@ -60,13 +65,20 @@ int main()
 
     // Setup fd=4 with valid file and all rights
     auto& fd4 = *env.fd_storage.opens.index_unchecked(4uz).fd_p;
-    fd4.file_fd = ::fast_io::native_file{u8"test_fd_fdstat_set_rights_wasm64.tmp",
-                                         ::fast_io::open_mode::out | ::fast_io::open_mode::creat | ::fast_io::open_mode::trunc};
+    fd4.wasi_fd.ptr->wasi_fd_storage.reset_type(::uwvm2::imported::wasi::wasip1::fd_manager::wasi_fd_type_e::file);
+    fd4.wasi_fd.ptr->wasi_fd_storage.storage
+        .file_fd
+#if defined(_WIN32) && !defined(__CYGWIN__)
+        .file
+#endif
+        = ::fast_io::native_file{u8"test_fd_fdstat_set_rights_wasm64.tmp",
+                                 ::fast_io::open_mode::out | ::fast_io::open_mode::creat | ::fast_io::open_mode::trunc};
     fd4.rights_base = static_cast<rights_wasm64_t>(-1);
     fd4.rights_inherit = static_cast<rights_wasm64_t>(-1);
 
     // Read back helper via fd_fdstat_get_wasm64
-    auto read_rights = [&] (wasi_posix_fd_wasm64_t fd, rights_wasm64_t& out_base, rights_wasm64_t& out_inh) {
+    auto read_rights = [&](wasi_posix_fd_wasm64_t fd, rights_wasm64_t& out_base, rights_wasm64_t& out_inh)
+    {
         constexpr wasi_void_ptr_wasm64_t stat_ptr{4096u};
         auto const gr = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_get_wasm64(env, fd, stat_ptr);
         if(gr != errno_wasm64_t::esuccess)
@@ -75,8 +87,12 @@ int main()
             ::fast_io::fast_terminate();
         }
         using u_rights = ::std::underlying_type_t<rights_wasm64_t>;
-        u_rights const base_u = ::uwvm2::imported::wasi::wasip1::memory::get_basic_wasm_type_from_memory_wasm64<u_rights>(memory, static_cast<wasi_void_ptr_wasm64_t>(stat_ptr + 8u));
-        u_rights const inh_u = ::uwvm2::imported::wasi::wasip1::memory::get_basic_wasm_type_from_memory_wasm64<u_rights>(memory, static_cast<wasi_void_ptr_wasm64_t>(stat_ptr + 16u));
+        u_rights const base_u =
+            ::uwvm2::imported::wasi::wasip1::memory::get_basic_wasm_type_from_memory_wasm64<u_rights>(memory,
+                                                                                                      static_cast<wasi_void_ptr_wasm64_t>(stat_ptr + 8u));
+        u_rights const inh_u =
+            ::uwvm2::imported::wasi::wasip1::memory::get_basic_wasm_type_from_memory_wasm64<u_rights>(memory,
+                                                                                                      static_cast<wasi_void_ptr_wasm64_t>(stat_ptr + 16u));
         out_base = static_cast<rights_wasm64_t>(base_u);
         out_inh = static_cast<rights_wasm64_t>(inh_u);
     };
@@ -84,7 +100,7 @@ int main()
     // Case 1: shrink base only (success)
     {
         rights_wasm64_t const new_base = rights_wasm64_t::right_fd_read;
-        rights_wasm64_t const new_inh = static_cast<rights_wasm64_t>(-1); // keep inheriting unchanged
+        rights_wasm64_t const new_inh = static_cast<rights_wasm64_t>(-1);  // keep inheriting unchanged
         auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env, static_cast<wasi_posix_fd_wasm64_t>(4), new_base, new_inh);
         if(ret != errno_wasm64_t::esuccess)
         {
@@ -106,8 +122,8 @@ int main()
         rights_wasm64_t curr_base{}, curr_inh{};
         read_rights(static_cast<wasi_posix_fd_wasm64_t>(4), curr_base, curr_inh);
 
-        rights_wasm64_t const new_base = curr_base;                 // unchanged
-        rights_wasm64_t const new_inh = static_cast<rights_wasm64_t>(0);   // shrink to zero
+        rights_wasm64_t const new_base = curr_base;                       // unchanged
+        rights_wasm64_t const new_inh = static_cast<rights_wasm64_t>(0);  // shrink to zero
         auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env, static_cast<wasi_posix_fd_wasm64_t>(4), new_base, new_inh);
         if(ret != errno_wasm64_t::esuccess)
         {
@@ -131,10 +147,7 @@ int main()
 
         rights_wasm64_t const new_base = static_cast<rights_wasm64_t>(static_cast<::std::underlying_type_t<rights_wasm64_t>>(curr_base) |
                                                                       static_cast<::std::underlying_type_t<rights_wasm64_t>>(rights_wasm64_t::right_fd_write));
-        auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env,
-                                                                                             static_cast<wasi_posix_fd_wasm64_t>(4),
-                                                                                             new_base,
-                                                                                             curr_inh);
+        auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env, static_cast<wasi_posix_fd_wasm64_t>(4), new_base, curr_inh);
         if(ret != errno_wasm64_t::enotcapable)
         {
             ::fast_io::io::perrln(::fast_io::u8err(), u8"fd_fdstat_set_rights_wasm64: increase base expected enotcapable");
@@ -150,10 +163,7 @@ int main()
         rights_wasm64_t const inc_inh = rights_wasm64_t::right_fd_write;
         rights_wasm64_t const new_inh = static_cast<rights_wasm64_t>(static_cast<::std::underlying_type_t<rights_wasm64_t>>(curr_inh) |
                                                                      static_cast<::std::underlying_type_t<rights_wasm64_t>>(inc_inh));
-        auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env,
-                                                                                             static_cast<wasi_posix_fd_wasm64_t>(4),
-                                                                                             curr_base,
-                                                                                             new_inh);
+        auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env, static_cast<wasi_posix_fd_wasm64_t>(4), curr_base, new_inh);
         if(ret != errno_wasm64_t::enotcapable)
         {
             ::fast_io::io::perrln(::fast_io::u8err(), u8"fd_fdstat_set_rights_wasm64: increase inheriting expected enotcapable");
@@ -164,13 +174,19 @@ int main()
     // Case 5: inheriting may contain bits that base does not (allowed by spec)
     {
         auto& fd5 = *env.fd_storage.opens.index_unchecked(5uz).fd_p;
-        fd5.file_fd = ::fast_io::native_file{u8"test_fd_fdstat_set_rights_wasm64_case5.tmp",
-                                             ::fast_io::open_mode::out | ::fast_io::open_mode::creat | ::fast_io::open_mode::trunc};
+        fd5.wasi_fd.ptr->wasi_fd_storage.reset_type(::uwvm2::imported::wasi::wasip1::fd_manager::wasi_fd_type_e::file);
+        fd5.wasi_fd.ptr->wasi_fd_storage.storage
+            .file_fd
+#if defined(_WIN32) && !defined(__CYGWIN__)
+            .file
+#endif
+            = ::fast_io::native_file{u8"test_fd_fdstat_set_rights_wasm64_case5.tmp",
+                                     ::fast_io::open_mode::out | ::fast_io::open_mode::creat | ::fast_io::open_mode::trunc};
         fd5.rights_base = static_cast<rights_wasm64_t>(0);
-        fd5.rights_inherit = rights_wasm64_t::right_fd_write; // inheriting not subset of base
+        fd5.rights_inherit = rights_wasm64_t::right_fd_write;  // inheriting not subset of base
 
-        rights_wasm64_t const new_base = fd5.rights_base;     // unchanged
-        rights_wasm64_t const new_inh = fd5.rights_inherit;   // unchanged
+        rights_wasm64_t const new_base = fd5.rights_base;    // unchanged
+        rights_wasm64_t const new_inh = fd5.rights_inherit;  // unchanged
 
         auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env, static_cast<wasi_posix_fd_wasm64_t>(5), new_base, new_inh);
         if(ret != errno_wasm64_t::esuccess)
@@ -191,8 +207,14 @@ int main()
     // Case 6: ebadf after fd_close
     {
         auto& fde = *env.fd_storage.opens.index_unchecked(2uz).fd_p;
-        fde.file_fd = ::fast_io::native_file{u8"test_fd_fdstat_set_rights_wasm64_close.tmp",
-                                             ::fast_io::open_mode::out | ::fast_io::open_mode::creat | ::fast_io::open_mode::trunc};
+        fde.wasi_fd.ptr->wasi_fd_storage.reset_type(::uwvm2::imported::wasi::wasip1::fd_manager::wasi_fd_type_e::file);
+        fde.wasi_fd.ptr->wasi_fd_storage.storage
+            .file_fd
+#if defined(_WIN32) && !defined(__CYGWIN__)
+            .file
+#endif
+            = ::fast_io::native_file{u8"test_fd_fdstat_set_rights_wasm64_close.tmp",
+                                     ::fast_io::open_mode::out | ::fast_io::open_mode::creat | ::fast_io::open_mode::trunc};
         fde.rights_base = static_cast<rights_wasm64_t>(-1);
         fde.rights_inherit = static_cast<rights_wasm64_t>(-1);
 
@@ -204,9 +226,9 @@ int main()
         }
 
         auto const ret = ::uwvm2::imported::wasi::wasip1::func::fd_fdstat_set_rights_wasm64(env,
-                                                                                             static_cast<wasi_posix_fd_wasm64_t>(2),
-                                                                                             static_cast<rights_wasm64_t>(0),
-                                                                                             static_cast<rights_wasm64_t>(0));
+                                                                                            static_cast<wasi_posix_fd_wasm64_t>(2),
+                                                                                            static_cast<rights_wasm64_t>(0),
+                                                                                            static_cast<rights_wasm64_t>(0));
         if(ret != errno_wasm64_t::ebadf)
         {
             ::fast_io::io::perrln(::fast_io::u8err(), u8"fd_fdstat_set_rights_wasm64: expected ebadf after fd_close");
@@ -214,5 +236,4 @@ int main()
         }
     }
 }
-
 
